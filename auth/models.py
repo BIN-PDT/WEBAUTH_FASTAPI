c1359646c9a7feb2b2d88a48feb0719055ac73e2
BIN BIN <thangpd300502@gmail.com>
2025-02-28 19:27:57 +0700

    UPDATE USER MODEL

--- auth/models.py
+++ auth/models.py
@@ -4,9 +4,9 @@ from datetime import datetime, timezone
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    username: str = Field(sa_type=String(150))
+    username: str = Field(sa_type=String(150), unique=True, index=True)
     password: str = Field(sa_type=String(150))
-    email: str = Field(sa_type=String(150))
+    email: str = Field(sa_type=String(150), unique=True, index=True)
     is_verified: bool = Field(default=False)
     is_active: bool = Field(default=True)
     date_joined: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
