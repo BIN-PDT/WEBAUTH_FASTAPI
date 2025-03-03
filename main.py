0f10443376b4aadf776264a40693f89649271a90
BIN BIN <thangpd300502@gmail.com>
2025-03-03 23:08:48 +0700

    CORS HANDLING

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from errors import BaseError
 from auth.routes import router
 
 
 app = FastAPI()
 app.include_router(router, prefix="/auth", tags=["auth"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=("*",),
+    allow_methods=("*",),
+    allow_headers=("*",),
+    allow_credentials=True,
+)
+app.add_middleware(
+    TrustedHostMiddleware,
+    allowed_hosts=("*",),
+    www_redirect=True,
+)
 
 
 @app.exception_handler(BaseError)
